61906c6eff154883cc138139aee7152014dfd34d
WhaleKimm <tmdals128551@gmail.com>
2025-03-13 16:23:53 +0900

    2-07 make questionList template

--- src/test/java/com/sb/sb/SbApplicationTests.java
+++ src/test/java/com/sb/sb/SbApplicationTests.java
@@ -11,6 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sb.sb.answer.Answer;
+import com.sb.sb.question.Question;
+import com.sb.sb.question.QuestionRepository;
+
 @SpringBootTest
 class SbApplicationTests {
 
@@ -20,13 +24,13 @@ class SbApplicationTests {
 	@Transactional
 	@Test
 	void testJpa() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
+		// Optional<Question> oq = this.questionRepository.findById(2);
+		// assertTrue(oq.isPresent());
+		// Question q = oq.get();
 
-		List<Answer> answerList = q.getAnswerList();
+		// List<Answer> answerList = q.getAnswerList();
 
-		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+		// assertEquals(1, answerList.size());
+		// assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 	}
 }
