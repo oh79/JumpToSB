151d96deeeafaee3b06334f36b48be36c4f9b510
oh79 <tmdals128551@gmail.com>
2025-04-01 22:54:24 +0900

    add login/logout func

--- src/main/java/com/sb/sb/user/UserRepository.java
+++ src/main/java/com/sb/sb/user/UserRepository.java
@@ -1,7 +1,9 @@
 package com.sb.sb.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long>{
-    
+    Optional<SiteUser> findByusername(String user);    
 }
